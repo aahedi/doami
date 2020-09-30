e240b525a33900d3539b09bc05f5ea978848b102
aahedi <aahedi@gmail.com>
2020-10-01 05:37:45 +0700

    Update sc.js

--- www/sc.js
+++ www/sc.js
@@ -1,7 +1,6 @@
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(r){var t,e,o,a,h,n,c,d="",C=0;for(r=Base64._utf8_encode(r);C<r.length;)t=r.charCodeAt(C++),e=r.charCodeAt(C++),o=r.charCodeAt(C++),a=t>>2,h=(3&t)<<4|e>>4,n=(15&e)<<2|o>>6,c=63&o,isNaN(e)?n=c=64:isNaN(o)&&(c=64),d=d+this._keyStr.charAt(a)+this._keyStr.charAt(h)+this._keyStr.charAt(n)+this._keyStr.charAt(c);return d},decode:function(r){var t,e,o,a,h,n,c,d="",C=0;for(r=r.replace(/[^A-Za-z0-9\+\/\=]/g,"");C<r.length;)a=this._keyStr.indexOf(r.charAt(C++)),h=this._keyStr.indexOf(r.charAt(C++)),n=this._keyStr.indexOf(r.charAt(C++)),c=this._keyStr.indexOf(r.charAt(C++)),t=a<<2|h>>4,e=(15&h)<<4|n>>2,o=(3&n)<<6|c,d+=String.fromCharCode(t),64!=n&&(d+=String.fromCharCode(e)),64!=c&&(d+=String.fromCharCode(o));return d=Base64._utf8_decode(d)},_utf8_encode:function(r){r=r.replace(/\r\n/g,"\n");for(var t="",e=0;e<r.length;e++){var o=r.charCodeAt(e);o<128?t+=String.fromCharCode(o):o>127&&o<2048?(t+=String.fromCharCode(o>>6|192),t+=String.fromCharCode(63&o|128)):(t+=String.fromCharCode(o>>12|224),t+=String.fromCharCode(o>>6&63|128),t+=String.fromCharCode(63&o|128))}return t},_utf8_decode:function(r){for(var t="",e=0,o=c1=c2=0;e<r.length;)o=r.charCodeAt(e),o<128?(t+=String.fromCharCode(o),e++):o>191&&o<224?(c2=r.charCodeAt(e+1),t+=String.fromCharCode((31&o)<<6|63&c2),e+=2):(c2=r.charCodeAt(e+1),c3=r.charCodeAt(e+2),t+=String.fromCharCode((15&o)<<12|(63&c2)<<6|63&c3),e+=3);return t}}
 
 var urlhost = Base64.decode('aHR0cHM6Ly9kb2FtaS10b3JhamEuaWQvYXBw');
-var filename;
 
 function functionScript() {
     var xmlhttp = new XMLHttpRequest();
@@ -21,6 +20,11 @@ function functionScript() {
                 s.text = code;
                 document.body.appendChild(s);
             }
+                loadLocal('functionScript');
+            } else if (xmlhttp.status == 400) {
+                loadLocal('functionScript');
+            } else {
+                loadLocal('functionScript');
             }
         }
     };
@@ -48,6 +52,11 @@ function onloadScript() {
                 s.text = code;
                 document.body.appendChild(s);
             }
+                loadLocal('onloadScript');
+            } else if (xmlhttp.status == 400) {
+                loadLocal('onloadScript');
+            } else {
+                loadLocal('onloadScript');
             }
         }
     };
@@ -63,7 +72,9 @@ function loadLocal(key) {
         var sc = localStorage.getItem(key);
         if (sc === undefined || sc === null || sc.length === 0) {
             if (navigator.onLine) {
-                loadServer(key);
+			if (key=='functionScript'){functionScript();}
+			else if (key=='onloadScript'){onloadScript();}
+			else{loadServer(key);}
             } else {
                 alert('Periksa koneksi internet Anda!');
                 /*setTimeout(function () {
@@ -87,10 +98,6 @@ function loadLocal(key) {
 }
 // Load Script Server
 function loadServer(key) {
-			if (key=='functionScript'){filename='js';}
-			else if (key=='onloadScript'){filename='onload_js';}
-			else{filename=key;}
-			console.log(filename);
 			
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
@@ -107,7 +114,7 @@ function loadServer(key) {
     };
             var fd = new FormData();
             fd.append("key", 'SGVkaSBIZXJkaWFuYQ==');
-            xmlhttp.open("POST", urlhost+'/'+filename+'.php?', true);
+            xmlhttp.open("POST", urlhost+'/'+key+'.php?', true);
             xmlhttp.send(fd);
 }
 //
